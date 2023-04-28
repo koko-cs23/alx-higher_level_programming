2acac9a9f9f94172d449eb87a811aaa6c3509232
koko-cs23 <dennisthebusinessguru@gmail.com>
2023-04-28 16:35:51 +0100

    fixed issues

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Check if the user has provided a URL
+if [ -z "$1" ]
+then
+    echo "Please provide a URL as an argument"
+    exit 1
+fi
+
+# Send a GET request to the URL with the header X-School-User-Id: 98
+curl -H "X-School-User-Id: 98" "$1"
